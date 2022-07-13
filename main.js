1674b66c2016b28f6a35eb275e91fef218d55591
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-13 16:39:05 +0900

    lec 8. Computed Properties

--- main.js
+++ main.js
@@ -6,18 +6,28 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       desc: 'This Socks are so comfortable',
-      image: './assets/images/socks_blue.jpg',
+      selectedVariant: 0,
       url_naver: 'https://www.naver.com',
-      inStock: false,
       onSale: true,
       inventory: 5,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+        },
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL'],
       cart: 0,
+      brand: 'Vue Mastery',
     };
   },
   methods: {
@@ -28,8 +38,22 @@ const app = Vue.createApp({
     subtractFromCart() {
       this.cart -= 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      return this.onSale ? `${this.brand} ${this.product} is on sale.` : '';
     },
   },
 });
