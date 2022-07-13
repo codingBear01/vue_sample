0ce7f1da4bed7a791217d60c0dbb3807bbad3d11
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-13 15:22:38 +0900

    lec 6. Event Handling

--- main.js
+++ main.js
@@ -13,10 +13,23 @@ const app = Vue.createApp({
       inventory: 5,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      // this means data
+      this.cart += 1;
+    },
+    subtractFromCart() {
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
