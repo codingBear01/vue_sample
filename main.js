e8fbb08c5388f4e422b418f12a9b1388c938fa5c
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-13 13:13:42 +0900

    lec 2: creating the Vue App

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+/* create Vue app */
+// Options obj for configuring Vue app(neccessary)
+const app = Vue.createApp({
+  // data() return full of data
+  data() {
+    return {
+      product: 'Socks',
+      desc: 'This Socks are so comfortable',
+    };
+  },
+});
