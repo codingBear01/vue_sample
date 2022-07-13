29ee0820ead710c6cfd01102ebf702eb6ccd5664
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-13 17:03:52 +0900

    lec 9. Components & Props

---
+++ components/ProductDisplay.js
@@ -0,0 +1,132 @@
+/* Vue Component */
+// first argument is the name of component, second argument is the obj to configure component
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template:
+    /*html*/
+    `<div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <!-- v-bind: dynamically bind an attribute  to an expression -->
+          <!-- you can write also like this for   dynamic attribute 
+            :src="image"
+            :class="image"...
+          -->
+          <img
+            v-bind:src="image"
+            alt="socks_green"
+            :class="[inStock ? '' :   'out-of-stock-img']"
+          />
+        </div>
+        <div class="product-info">
+          <!-- inside of double curly brace refers  product property in Vue app -->
+          <h1 v-show="onSale">{{ title }}</h1>
+          <p>{{ sale }}</p>
+          <!-- if inStock is true, show the first p   tag, otherwise show the second p tag -->
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+
+          <p>Shipping: {{ shipping }}</p>
+
+          <!-- v-show is used for toggling in el's  visibility. if value is false,   display:none will be added -->
+          <!-- <p v-show="inStock">In Stock</p> -->
+  
+          <!-- you can use multiple conditions -->
+          <p v-if="inventory > 10">In Stock {{  inventory }}</p>
+          <p v-else-if="inventory <= 10 && inventory  > 0">
+            Almost sold out {{ inventory }}
+          </p>
+          <p v-else>10">Out of Stock</p>
+          <p v-if="onSale">On Sale</p>
+          <p>{{ desc }}</p>
+          <product-details :details="details"></product-details>
+          <ul>
+            <li v-for="size in sizes">{{ size }}</li>
+          </ul>
+          <!-- @mouseover = hover event -->
+          <!-- you should write style property name   by camel case cuz that's javascript or  kebab case like this 'background-color' -->
+          <div
+            v-for="(variant, index) in variants"
+            :key="variant.id"
+            @mouseover="updateVariant(index)"
+            class="color-circle"
+            :style="{ backgroundColor: variant.color} "
+          ></div>
+          <!-- v-on listens to events(ex. click)
+          you can write like this @click='method' -->
+          <!-- :class is applied whenever !inStock  -->
+          <button
+            class="button"
+            :class="{ disabledButton: !inStock}"
+            :disabled="!inStock"
+            v-on:click="addToCart"
+          >
+            Add to Cart
+          </button>
+          <button class="button"  v-on:click="subtractFromCart">
+            Subtract from Cart
+          </button>
+        </div>
+      </div>
+    </div>`,
+  data() {
+    return {
+      product: 'Socks',
+      desc: 'This Socks are so comfortable',
+      selectedVariant: 0,
+      onSale: true,
+      inventory: 5,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+        },
+      ],
+      sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+      brand: 'Vue Mastery',
+    };
+  },
+  methods: {
+    addToCart() {
+      // this means data
+      this.cart += 1;
+    },
+    subtractFromCart() {
+      this.cart -= 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      return this.onSale ? `${this.brand} ${this.product} is on sale.` : '';
+    },
+    shipping() {
+      return this.premium ? 'Free' : '$2.99';
+    },
+  },
+});
--- main.js
+++ main.js
@@ -1,59 +1,13 @@
-/* create Vue app */
+/* Vue App */
 // Options obj for configuring Vue app(neccessary)
 const app = Vue.createApp({
   // data() return full of data
   data() {
     return {
-      product: 'Socks',
-      desc: 'This Socks are so comfortable',
-      selectedVariant: 0,
-      url_naver: 'https://www.naver.com',
-      onSale: true,
-      inventory: 5,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        {
-          id: 2234,
-          color: 'green',
-          image: './assets/images/socks_green.jpg',
-          quantity: 50,
-        },
-        {
-          id: 2235,
-          color: 'blue',
-          image: './assets/images/socks_blue.jpg',
-          quantity: 0,
-        },
-      ],
-      sizes: ['S', 'M', 'L', 'XL', 'XXL'],
       cart: 0,
-      brand: 'Vue Mastery',
+      url_naver: 'https://www.naver.com',
+      premium: false,
     };
   },
-  methods: {
-    addToCart() {
-      // this means data
-      this.cart += 1;
-    },
-    subtractFromCart() {
-      this.cart -= 1;
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity;
-    },
-    sale() {
-      return this.onSale ? `${this.brand} ${this.product} is on sale.` : '';
-    },
-  },
+  methods: {},
 });
